a28a60116897c2a5e9dbf28a96a8f228c16053d6
Emanuele Disco <emawind84@gmail.com>
2015-08-21 00:00:20 +0900

    modified conf.py for discodev

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'SangAh Development Note'
+project = u'Disco Development Note'
 copyright = u'2015, Disco Emanuele'
 author = u'Disco Emanuele'
 
@@ -129,7 +129,7 @@ html_theme_path = ["_themes"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_images/logo.gif'
+#html_logo = '_images/logo.gif'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -224,7 +224,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'DiscoSphinxTest.tex', u'SangAh Development Note',
+  (master_doc, 'DiscoSphinxTest.tex', u'Disco Dev Note',
    u'Disco Emanuele', 'manual'),
 ]
 
@@ -254,7 +254,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'discosphinxtest', u'SangAh Development Note',
+    (master_doc, 'discosphinxtest', u'Disco Development Note',
      [author], 1)
 ]
 
@@ -268,7 +268,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'DiscoSphinxTest', u'SangAh Development Note',
+  (master_doc, 'DiscoSphinxTest', u'Disco Development Note',
    author, 'DiscoSphinxTest', 'One line description of project.',
    'Miscellaneous'),
 ]
